bb9b38a41c0d716f92d96b434b247dc553c07698
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-03-18 16:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let totalSum = 0;
+  const prices = document.querySelectorAll(".price");
+  prices.forEach((e) => {
+    totalSum += Number(e.innerText);
+  });
+
+  const table = document.querySelector("table");
+  const row = document.createElement("tr");
+  const cell1 = document.createElement("td");
+  const cell2 = document.createElement("td");
+  cell1.innerText = "Total Price";
+  cell2.innerText = totalSum;
+  cell2.id = "ans";
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  table.appendChild(row);
   
 };
 
